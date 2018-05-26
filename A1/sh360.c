53e08a5ef73265cecb1d5bcb106df195343284e0
Andrea <andrea.k.williams7@gmail.com>
2018-05-26 16:29:03 -0700

    Reads in first line of config file; uses it as command prompt

--- A1/sh360.c
+++ A1/sh360.c
@@ -3,10 +3,12 @@
  * CSC 360, Summer 2018
  *
  */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
 
 /*
  * set global variables
@@ -15,14 +17,53 @@
 #define MAX_INPUT_LINE 80
 #define MAX_LEN_PROMPT 10
 #define MAX_NUM_DIRS 10
+#define CONFIG_FILENAME "./.uvshrc"
 
+/*
+ * reads first line of .sh360rc file to get string 
+ * to be used as command prompt
+ */
+/*
+ * this function has been adapted from code given as example in
+ * https://www.tutorialspoint.com/c_standard_library/c_function_fopen.htm
+ * and from example in 
+ * http://man7.org/linux/man-pages/man3/getline.3.html
+ */
+char* getCommandPrompt() {
+    FILE * fp;
+    char * line = NULL;
+    char * prompt = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    fp = fopen(CONFIG_FILENAME, "r");
+
+    if (fp == NULL)
+    {
+        fprintf(stderr, "Error: cannot open file %s\n", CONFIG_FILENAME);
+        exit(1);
+    }
+
+    while( (read = getline(&line, &len, fp)) != -1 ) {
+        prompt = line;
+        prompt[strlen(prompt)-1] = 0;
+        break;   
+    }
+
+    fclose(fp);
+    free(line);
+    return prompt;
+}
 
 int main(int argc, char *argv[]) {
     char input[MAX_INPUT_LINE];
     int  line_len;
 
+    char* commandPrompt = getCommandPrompt();
+
+    // infinite loop to accept user input
     for(;;) {
-        fprintf(stdout, "> ");
+        fprintf(stdout, "%s ", commandPrompt);
         fflush(stdout);
         fgets(input, MAX_INPUT_LINE, stdin);
         if (input[strlen(input) - 1] == '\n') {
