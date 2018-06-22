a5f9bfa48a7ad21c67f5aa2ae568b4ce9fa3a97a
Andrea <andrea.k.williams7@gmail.com>
2018-06-21 18:27:27 -0700

    Made writers-priority solution to read/write problem

--- a2/rw.c
+++ a2/rw.c
@@ -9,13 +9,18 @@
 #include "resource.h"
 
 /*
+ *   This solution is based on the algorithm presented in Doeppner pp.67-68
+ *   as a solution to the readers-writers problem
+ */
+
+/*
  * Declarations for reader-writer shared variables -- plus concurrency-control
  * variables -- must START here.
  */
-
 static resource_t data;
 int readers = 0;
 int writers = 0;
+int active_writers = 0;
 pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t readerQ = PTHREAD_COND_INITIALIZER;
 pthread_cond_t writerQ = PTHREAD_COND_INITIALIZER;
@@ -46,15 +51,20 @@ void rw_read(char *value, int len) {
 
 void rw_write(char *value, int len) {
     pthread_mutex_lock(&m);
-    while(!((readers == 0) && (writers == 0))) {
+    writers++;
+    while(!((readers == 0) && (active_writers == 0))) {
     	pthread_cond_wait(&writerQ, &m);
     }
-    writers++;
+    active_writers++;
     pthread_mutex_unlock(&m);
     write_resource(&data, value, len);
     pthread_mutex_lock(&m);
-    writers--;
-    pthread_cond_signal(&writerQ);
-    pthread_cond_broadcast(&readerQ);
+    active_writers--;
+    if (--writers == 0) {
+        pthread_cond_broadcast(&readerQ);	
+    }
+    else {
+        pthread_cond_signal(&writerQ);    	
+    }
     pthread_mutex_unlock(&m);
 }
